6aeed1c594ea649e149dd3d7c5fc3568b07aada1
Katia Ghezali <ghezalikatia@hotmail.com>
2023-08-13 20:31:40 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+
+function findMatching(drivers , name){
+    return drivers.filter(function(driver) {
+        return driver.toLowerCase() === name.toLowerCase();
+    });
+}
+
+function fuzzyMatch(drivers , letter){
+    return drivers.filter(function(driver) {
+        for( let i = 0 ; i < letter.length ; i++){
+            if(driver[i] !== letter[i]) {
+                return false
+            }
+        }
+        
+        return true
+    });
+}
+
+function matchName(drivers , nameGiven) {
+    return drivers.filter(function(driver) {
+         
+         return driver.name === nameGiven
+    });
+
+}
